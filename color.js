8fb16a6bb642784df1c218497eff0c626d332945
Jeet Shah <jeetshah4321@gmail.com>
2017-08-06 23:36:27 -0400

    added levels

--- color.js
+++ color.js
@@ -1,23 +1,59 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var correctColor = pickColor();
 var colorDisplay = document.querySelector("#colorString");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var newColors = document.querySelector("#newColors");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
+
 
 colorDisplay.textContent = correctColor;
 
 newColors.addEventListener("click", function(){
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     correctColor = pickColor();
     colorDisplay.textContent = correctColor;
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
     h1.style.backgroundColor = "#232323";
+    this.textContent = "New Colors";
 });
+
+easyButton.addEventListener("click", function(){
+    hardButton.classList.remove("selected");
+    this.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    correctColor = pickColor();
+    colorDisplay.textContent = correctColor;
+
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        }else{
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardButton.addEventListener("click", function(){
+    easyButton.classList.remove("selected");
+    this.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    correctColor = pickColor();
+    colorDisplay.textContent = correctColor;
+
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
 
 for(var i = 0; i<squares.length; i++){
     //add initial colors
