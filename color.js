ef676620477aa04e1d13c97a759cb7a855c2f056
Jeet Shah <jeetshah4321@gmail.com>
2017-08-06 22:38:37 -0400

    Random generated array of colors

--- color.js
+++ color.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var correctColor = pickColor();
 var colorDisplay = document.querySelector("#colorString");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = correctColor;
 
@@ -23,6 +17,7 @@ for(var i = 0; i<squares.length; i++){
         if(selected === correctColor){
             messageDisplay.textContent = "Correct!";
             changeColors(correctColor);
+            h1.style.backgroundColor = correctColor;
         }
         else{
             this.style.backgroundColor = "#232323";
@@ -36,7 +31,30 @@ function changeColors(color){
         squares[i].style.backgroundColor = color;
     }
 }
+
 function pickColor(){
-    var random =  Math.floor(Math.random() * colors.length + 1);
+    var random =  Math.floor(Math.random() * colors.length);
     return colors[random];
 }
+
+function generateRandomColors(num){
+    //make array
+    var arr = [];
+    //add num random colors to array
+    for(var i=0; i<num; i++){
+        arr.push(randomColor());
+    }
+    //return array
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 255 + 1);
+    var g = Math.floor(Math.random() * 255 + 1);
+    var b = Math.floor(Math.random() * 255 + 1);
+
+    var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+
+    return rgb;
+}
+
