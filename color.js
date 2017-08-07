1b32a908b1ce6061b353186cebbb3dce0d11618d
Jeet Shah <jeetshah4321@gmail.com>
2017-08-07 00:01:06 -0400

    Added styling for page

--- color.js
+++ color.js
@@ -19,7 +19,7 @@ newColors.addEventListener("click", function(){
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
     this.textContent = "New Colors";
 });
 
